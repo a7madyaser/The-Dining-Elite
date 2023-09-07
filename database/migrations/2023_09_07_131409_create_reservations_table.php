0493be9cdfd74b5eef08ae6e5b3d2f961b221ccd
Maryana Alhawamdah <maryana.alhawamdah8@gmail.com>
2023-09-07 21:07:02 +0300

    data

---
+++ database/migrations/2023_09_07_131409_create_reservations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
+            $table->datetime('res_date');
+            $table->Integer('user_id');
+            $table->Integer('guest_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
